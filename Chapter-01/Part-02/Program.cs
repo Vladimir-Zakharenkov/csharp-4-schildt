97db14b6f3366a9d33432e2fcee1bb0fdfb08295
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-21 09:38:33 +0300

    Chapter-01 Part-02

---
+++ Chapter-01/Part-02/Program.cs
@@ -0,0 +1,77 @@
+﻿#region Russian
+
+/*
+
+Появление ООП и C++
+
+К концу 1970-х годов масштабы многих проектов приблизились к пределам, с которыми
+уже не могли справиться методики структурного программирования вообще
+и язык С в частности. Для решения этой проблемы было открыто новое направление
+в программировании — так называемое объектно-ориентированное программирование
+(ООП). Применяя метод ООП, программист мог работать с более "крупными"
+программами. Но главная трудность заключалась в том, что С, самый распространенный
+в то время язык, не поддерживал ООП. Стремление к созданию объектно-
+ориентированного варианта С в конечном итоге привело к появлению C++.
+
+Язык C++ был разработан в 1979 году Бьярне Страуструпом (Bjarne Stroustrup), работавшим
+в компании Bell Laboratories, базировавшейся в Мюррей-Хилл, шт. Нью-Джерси. 
+Первоначально новый язык назывался "С с классами", но в 1983 году он был
+переименован в C++. Язык С полностью входит в состав C++, а следовательно, С служит
+основанием, на котором зиждется C++. Большая часть дополнений, введенных Страуструпом,
+обеспечивала плавный переход к ООП. И вместо того чтобы изучать совершенно
+новый язык, программирующему на С требовалось лишь освоить ряд новых
+свойств, чтобы воспользоваться преимуществами методики ООП.
+
+В течение 1980-х годов C++ все еще оставался в тени, интенсивно развиваясь, но к началу
+1990-х годов, когда он уже был готов для широкого применения, его популярность
+в области программирования заметно возросла. К концу 1990-х годов он стал
+наиболее широко распространенным языком программирования и в настоящее время
+по-прежнему обладает неоспоримыми преимуществами языка разработки высокопроизводительных
+программ системного уровня.
+
+Важно понимать, что разработка C++ не была попыткой создать совершенно новый
+язык программирования. Напротив, это была попытка усовершенствовать уже существовавший
+довольно удачный язык. Такой подход к разработке языков программирования,
+основанный на уже существующем языке и совершенствующий его далее,
+превратился в упрочившуюся тенденцию, которая продолжается до сих пор.
+
+*/
+
+#endregion
+
+#region English
+
+/*
+
+The Creation of OOP and C++
+
+By the late 1970s, the size of many projects was near or at the limits of what structured
+programming methodologies and the C language could handle. To solve this problem, a
+new way to program began to emerge. This method is called object-oriented programming
+(OOP). Using OOP, a programmer could handle much larger programs. The trouble was
+that C, the most popular language at the time, did not support object-oriented programming.
+The desire for an object-oriented version of C ultimately led to the creation of C++.
+
+C++ was invented by Bjarne Stroustrup beginning in 1979 at Bell Laboratories in Murray
+Hill, New Jersey. He initially called the new language “C with Classes.” However, in 1983 the
+name was changed to C++. C++ contains the entire C language. Thus, C is the foundation
+upon which C++ is built. Most of the additions that Stroustrup made to C were designed to
+support object-oriented programming. In essence, C++ is the object-oriented version of C.
+By building upon the foundation of C, Stroustrup provided a smooth migration path to OOP.
+Instead of having to learn an entirely new language, a C programmer needed to learn only
+a few new features before reaping the benefits of the object-oriented methodology.
+
+C++ simmered in the background during much of the 1980s, undergoing extensive
+development. By the beginning of the 1990s, C++ was ready for mainstream use, and its
+popularity exploded. By the end of the decade, it had become the most widely used
+programming language. Today, C++ is still the preeminent language for the development of
+high-performance system code.
+
+It is critical to understand that the invention of C++ was not an attempt to create an
+entirely new programming language. Instead, it was an enhancement to an already highly
+successful language. This approach to language development—beginning with an existing
+language and moving it forward—established a trend that continues today.
+
+*/
+
+#endregion
