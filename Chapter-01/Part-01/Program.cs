97ef303839045c7c31fcece50e1a66c0987786da
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-21 09:41:28 +0300

    Chapter-01 Part-01

--- Chapter-01/Part-01/Program.cs
+++ Chapter-01/Part-01/Program.cs
@@ -75,3 +75,68 @@ XXI века.
 */
 
 #endregion
+
+#region English
+
+/*
+
+CHAPTER 1
+
+The Creation of C#
+
+C# is Microsoft’s premier language for .NET development. It leverages time-tested
+features with cutting-edge innovations and provides a highly usable, efficient way
+to write programs for the modern enterprise computing environment. It is, by any
+measure, one of the most important languages of the twenty-first century.
+
+The purpose of this chapter is to place C# into its historical context, including the forces
+that drove its creation, its design philosophy, and how it was influenced by other computer
+languages. This chapter also explains how C# relates to the .NET Framework. As you will
+see, C# and the .NET Framework work together to create a highly refined programming
+environment.
+
+C#’s Family Tree
+
+Computer languages do not exist in a void. Rather, they relate to one another, with each
+new language influenced in one form or another by the ones that came before. In a process
+akin to cross-pollination, features from one language are adapted by another, a new
+innovation is integrated into an existing context, or an older construct is removed. In
+this way, languages evolve and the art of programming advances. C# is no exception.
+
+C# inherits a rich programming legacy. It is directly descended from two of the world’s
+most successful computer languages: C and C++. It is closely related to another: Java.
+Understanding the nature of these relationships is crucial to understanding C#. Thus, we
+begin our examination of C# by placing it in the historical context of these three languages.
+
+C: The Beginning of the Modern Age of Programming
+
+The creation of C marks the beginning of the modern age of programming. C was invented
+by Dennis Ritchie in the 1970s on a DEC PDP-11 that used the UNIX operating system.
+While some earlier languages, most notably Pascal, had achieved significant success, it
+was C that established the paradigm that still charts the course of programming today.
+
+C grew out of the structured programming revolution of the 1960s. Prior to structured
+programming, large programs were difficult to write because the program logic tended
+to degenerate into what is known as “spaghetti code,” a tangled mass of jumps, calls, and
+returns that is difficult to follow. Structured languages addressed this problem by adding
+well-defined control statements, subroutines with local variables, and other improvements.
+Through the use of structured techniques programs became better organized, more reliable,
+and easier to manage.
+
+Although there were other structured languages at the time, C was the first to successfully
+combine power, elegance, and expressiveness. Its terse, yet easy-to-use syntax coupled with
+its philosophy that the programmer (not the language) was in charge quickly won many
+converts. It can be a bit hard to understand from today’s perspective, but C was a breath of
+fresh air that programmers had long awaited. As a result, C became the most widely used
+structured programming language of the 1980s.
+
+However, even the venerable C language had its limits. One of the most troublesome
+was its inability to handle large programs. The C language hits a barrier once a project
+reaches a certain size, and after that point, C programs are difficult to understand and
+maintain. Precisely where this limit is reached depends upon the program, the programmer,
+and the tools at hand, but there is always a threshold beyond which a C program becomes
+unmanageable.
+
+*/
+
+#endregion
