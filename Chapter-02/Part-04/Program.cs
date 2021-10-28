efd9fec8eef6ba1eb9056e8bc36e726af77753a7
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-28 20:44:10 +0300

    Chapter-02 Part-04

---
+++ Chapter-02/Part-04/Program.cs
@@ -0,0 +1,58 @@
+﻿#region Russian
+
+/*
+
+Наследование
+
+Наследование представляет собой процесс, в ходе которого один объект приобретает
+свойства другого объекта. Это очень важный процесс, поскольку он обеспечивает
+принцип иерархической классификации. Если вдуматься, то большая часть знаний
+поддается систематизации благодаря иерархической классификации по нисходящей.
+Например, сорт яблок "Джонатан" входит в общую классификацию сортов яблок,
+которые, в свою очередь, относятся к классу фруктов, а те — к еще более крупному
+классу пищевых продуктов. Это означает, что класс пищевых продуктов обладает рядом
+свойств (съедобности, питательности и т.д.), которые по логике вещей распространяются
+и на его подкласс фруктов. Помимо этих свойств, класс фруктов обладает своими
+собственными свойствами (сочностью, сладостью и т.д.), которыми он отличается
+от других пищевых продуктов. У класса яблок имеются свои характерные особенности
+(растут на деревьях, не в тропиках и т.д.). Таким образом, сорт яблок "Джонатан" наследует
+свойства всех предшествующих классов, обладая в то же время свойствами,
+присущими только этому сорту яблок, например красной окраской кожицы с желтым
+бочком и характерным ароматом и вкусом.
+
+Если не пользоваться иерархиями, то для каждого объекта пришлось бы явно определять
+все его свойства. А если воспользоваться наследованием, то достаточно определить
+лишь те свойства, которые делают объект особенным в его классе. Он может также
+наследовать общие свойства своего родителя. Следовательно, благодаря механизму
+наследования один объект становится отдельным экземпляром более общего класса.
+
+*/
+
+#endregion
+
+#region English
+
+/*
+
+Inheritance
+
+Inheritance is the process by which one object can acquire the properties of another object.
+This is important because it supports the concept of hierarchical classification. If you
+think about it, most knowledge is made manageable by hierarchical (that is, top-down)
+classifications. For example, a Red Delicious apple is part of the classification apple, which
+in turn is part of the fruit class, which is under the larger class food. That is, the food class
+possesses certain qualities (edible, nutritious, and so on) which also, logically, apply to its
+subclass, fruit. In addition to these qualities, the fruit class has specific characteristics (juicy,
+sweet, and so on) that distinguish it from other food. The apple class defines those qualities
+specific to an apple (grows on trees, not tropical, and so on). A Red Delicious apple would,
+in turn, inherit all the qualities of all preceding classes and would define only those qualities
+that make it unique.
+
+Without the use of hierarchies, each object would have to explicitly define all of its
+characteristics. Using inheritance, an object need only define those qualities that make it
+unique within its class. It can inherit its general attributes from its parent. Thus, the
+inheritance mechanism makes it possible for one object to be a specific instance of a more
+general case.
+*/
+
+#endregion
