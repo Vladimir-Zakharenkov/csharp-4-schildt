10e504963abfaca1c5575d153a863328edb0ceb8
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-11-06 11:36:58 +0300

    Chapter-02 Part-05

---
+++ Chapter-02/Part-05/Program.cs
@@ -0,0 +1,76 @@
+﻿#region Russian
+
+/*
+
+А теперь самое время перейти к примеру конкретной программы на С#. Для начала
+скомпилируем и выполним короткую программу.
+
+*/
+
+// Это простая программа на С#. Назовем ее Example.cs.
+
+using System;
+
+class Example
+{
+    // Любая программа на C# начинается с вызова метода Main().
+
+    static void Main()
+    {
+        Console.WriteLine("Простая программа на C#.");
+    }
+}
+
+/*
+
+Основной средой для разработки программ на C# служит Visual Studio корпорации
+Microsoft. Для компилирования примеров всех программ, приведенных для примера
+в этой книге, в том числе и тех, где используются новые средства C# 4.0, вам потребуется
+Visual Studio 2010 или же более поздняя версия, поддерживающая С#.
+
+Создавать, компилировать и выполнять программы на С#, используя Visual Studio,
+можно двумя способами: пользуясь, во-первых, интегрированной средой разработки
+Visual Studio, а во-вторых, — компилятором командной строки csc.ехе. Далее описываются
+оба способа.
+
+*/
+
+#endregion
+
+#region English
+
+/*
+
+A First Simple Program
+
+It is now time to look at an actual C# program. We will begin by compiling and running the
+short program shown next.
+
+*/
+
+// This is a simple C# program. Call this program Example.cs.
+
+//using System;
+
+//class Example
+//{
+//    // A C# program begins with a call to Main().
+//    static void Main()
+//    {
+//        Console.WriteLine("A simple C# program.");
+//    }
+//}
+
+/*
+
+The primary development environment for C# is Microsoft’s Visual Studio. To compile
+all of the programs in this book, including those that use the new C# 4.0 features, you will
+need to use a version of Visual Studio 2010 (or later) that supports C#.
+
+Using Visual Studio, there are two general approaches that you can take to creating,
+compiling, and running a C# program. First, you can use the Visual Studio IDE. Second,
+you can use the command-line compiler, csc.exe. Both methods are described here.
+
+*/
+
+#endregion
