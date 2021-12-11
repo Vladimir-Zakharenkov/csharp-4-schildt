f8b4216a020fd2eeaa1de90ce01e6bcaebcab91f
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-12-12 00:13:14 +0300

    Chapter-02 Part-18

---
+++ Chapter-02/Part-18/Program.cs
@@ -0,0 +1,105 @@
+﻿#region Russian
+
+/*
+
+Ключевые слова C#
+
+Основу любого языка программирования составляют его ключевые слова, поскольку
+они определяют средства, встроенные в этот язык. В C# определены два общих типа
+ключевых слов: зарезервированные и контекстные. Зарезервированные ключевые слова
+нельзя использовать в именах переменных, классов или методов. Их можно использовать
+только в качестве ключевых слов. Именно поэтому они и называются зарезервированными.
+Их иногда еще называют зарезервированными словами, или зарезервированными
+идентификаторами. В настоящее время в версии 4.0 языка С# определено 77 зарезервированных
+ключевых слов (табл. 2.1).
+
+Таблица 2.1. Ключевые слова, зарезервированные в языке C#
+
+abstract        as          base        bool        break
+byte            case        catch       char        checked
+class           const       continue    decimal     default
+delegate        do          double      else        enum
+event           explicit    extern      false       finally
+fixed           float       for         foreach     goto
+if              implicit    in          int         interface
+internal        is          lock        long        namespace
+new             null        object      operator    out
+override        params      private     protected   public
+readonly        ref         return      sbyte       sealed
+short           sizeof      stackalloc  static      string
+struct          switch      this        throw       true
+try             typeof      uint        ulong       unchecked
+unsafe          ushort      using       virtual     volatile
+void            while
+
+Кроме того, в версии C# 4.0 определены 18 контекстных ключевых слов, которые
+приобретают особое значение в определенном контексте. В таком контексте они выполняют
+роль ключевых слов, а вне его они могут использоваться в именах других
+элементов программы, например в именах переменных. Следовательно, контекстные
+ключевые слова формально не являются зарезервированными. Но, как правило, их следует
+считать зарезервированными, избегая их применения в любых других целях. Ведь
+применение контекстного ключевого слова в качестве имени какого-нибудь другого
+элемента программы может привести к путанице, и поэтому считается многими программистами
+плохой практикой. Контекстные ключевые слова приведены в табл. 2.2.
+
+Таблица 2.2. Контекстные ключевые слова в C#
+
+add         dynamic     from        get     global
+group       into        join        let     orderby
+partial     remove      select      set     value
+var         where       yield
+
+*/
+
+#endregion
+
+#region English
+
+/*
+
+The C# Keywords
+
+At its foundation, a computer language is defined by its keywords because they determine
+the features built into the language. C# defines two general types of keywords: reserved and
+contextual. The reserved keywords cannot be used as names for variables, classes, or methods.
+They can be used only as keywords. This is why they are called reserved. The terms reserved
+words or reserved identifiers are also sometimes used. There are currently 77 reserved keywords
+defined by version 4.0 of the C# language. They are shown in Table 2-1.
+
+C# 4.0 defines 18 contextual keywords that have a special meaning in certain contexts. In
+those contexts, they act as keywords. Outside those contexts, they can be used as names for
+other program elements, such as variable names. Thus, they are not technically reserved. As
+a general rule, however, you should consider the contextual keywords reserved and avoid
+using them for any other purpose. Using a contextual keyword as a name for some other
+program element can be confusing and is considered bad practice by many programmers.
+The contextual keywords are shown in Table 2-2.
+
+abstract        as          base        bool        break
+byte            case        catch       char        checked
+class           const       continue    decimal     default
+delegate        do          double      else        enum
+event           explicit    extern      false       finally
+fixed           float       for         foreach     goto
+if              implicit    in          int         interface
+internal        is          lock        long        namespace
+new             null        object      operator    out
+override        params      private     protected   public
+readonly        ref         return      sbyte       sealed
+short           sizeof      stackalloc  static      string
+struct          switch      this        throw       true
+try             typeof      uint        ulong       unchecked
+unsafe          ushort      using       virtual     volatile
+void            while
+
+TABLE 2-1 The C# Reserved Keywords
+
+add         dynamic     from        get     global
+group       into        join        let     orderby
+partial     remove      select      set     value
+var         where       yield
+
+TABLE 2-2 The C# Contextual Keywords
+
+*/
+
+#endregion
