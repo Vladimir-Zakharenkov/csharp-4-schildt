2dade283f6d58d54c822a6493677e70eb691a4af
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-04-14 00:00:08 +0300

    Chapter-24 Part-09 - Part-10

---
+++ Chapter-24/Part-09/Program.cs
@@ -0,0 +1,76 @@
+﻿#region Russian
+
+/*
+
+Вызов метода Dispose()
+
+В классе Task реализуется интерфейс IDisposable, в котором определяется метод
+Dispose(). Ниже приведена форма его объявления.
+
+public void Dispose()
+
+Метод Dispose() реализуется в классе Task, освобождая ресурсы, используемые
+этим классом. Как правило, ресурсы, связанные с классом Task, освобождаются автоматически
+во время "сборки мусора" (или по завершении программы). Но если эти
+ресурсы требуется освободить еще раньше, то для этой цели служит метод Dispose().
+Это особенно важно в тех программах, где создается большое число задач, оставляемых
+на произвол судьбы.
+
+Следует, однако, иметь в виду, что метод Dispose() можно вызывать для отдельной
+задачи только после ее завершения. Следовательно, для выяснения факта завершения
+отдельной задачи, прежде чем вызывать метод Dispose(), потребуется некоторый
+механизм, например, вызов метода Wait(). Именно поэтому так важно было
+рассмотреть метод Wait(), перед тем как обсуждать метод Dispose(). Ели же попытаться
+вызвать Dispose() для все еще активной задачи, то будет сгенерировано исключение
+InvalidOperationException.
+
+Во всех примерах, приведенных в этой главе, создаются довольно короткие задачи,
+которые сразу же завершаются, и поэтому применение метода Dispose() в этих примерах
+не дает никаких преимуществ. (Именно по этой причине вызывать метод Dispose()
+в приведенных выше программах не было никакой необходимости. Ведь все они завершались,
+как только завершалась задача, что в конечном итоге приводило к освобождению от
+остальных задач.) Но в целях демонстрации возможностей данного метода и во избежание
+каких-либо недоразумений метод Dispose() будет вызываться явным образом при
+непосредственном обращении с экземплярами объектов типа Task во всех последующих
+примерах программ. Если вы обнаружите отсутствие вызовов метода Dispose() в исходном
+коде, полученном из других источников, то не удивляйтесь этому. Опять же, если
+программа завершается, как только завершится задача, то вызывать метод Dispose() нет
+никакого смысла — разве что в целях демонстрации его применения.
+
+*/
+
+#endregion
+
+#region English
+
+/*
+
+Calling Dispose()
+
+The Task class implements the IDisposable interface, which specifies the Dispose()
+method. It has this form:
+
+public void Dispose()
+
+As implemented by Task, Dispose() releases the resources used by the Task. In general, the
+resources associated with a Task are automatically released when the Task is subjected to
+garbage collection (or when the program terminates). However, to release those resources
+before then, call Dispose(). This is especially important in a program in which large
+numbers of tasks are created and then abandoned.
+It is important to understand that Dispose() can be called on a task only after it has
+completed. Thus, you will need to use some mechanism, such as Wait(), to determine that
+a task has completed before calling Dispose(). This is why it was necessary to describe the
+Wait() method prior to discussing Dispose(). It you do try to call Dispose() on a still active
+task, an InvalidOperationException will be generated.
+Because all of the examples in this chapter create few tasks, are quite short, and end
+immediately, calls to Dispose() are of essentially no benefit. (This is why it was not
+necessary to call Dispose() in the preceding programs; they all end as soon as the tasks end,
+thus resulting in the disposal of the tasks.) However, so as to demonstrate its use and to
+avoid confusion in this regard, all subsequent examples will call Dispose() explicitly when
+working directly with Task instances. However, don’t be surprised if you see example code
+from other sources that do not. Again, if a program will be ending as soon as a task ends,
+then there is essentially no point is calling Dispose()—aside from demonstrating its use.
+
+*/
+
+#endregion
---
+++ Chapter-24/Part-10/Program.cs
@@ -0,0 +1,147 @@
+﻿#region Russian
+
+/*
+
+Применение класса TaskFactory для запуска задачи
+
+Приведенные выше примеры программы были составлены не так эффективно, как
+следовало бы, поскольку задачу можно создать и сразу же начать ее исполнение, вызвав
+метод StartNew(), определенный в классе TaskFactory. В классе TaskFactory
+предоставляются различные методы, упрощающие создание задач и управление ими.
+По умолчанию объект класса TaskFactory может быть получен из свойства Factory,
+доступного только для чтения в классе Task. Используя это свойство, можно вызвать
+любые методы класса TaskFactory. Метод StartNew() существует во множестве
+форм. Ниже приведена самая простая форма его объявления:
+
+public Task StartNew(Action action)
+
+где action — точка входа в исполняемую задачу. Сначала в методе StartNew() автоматически
+создается экземпляр объекта типа Task для действия, определяемого параметром
+action, а затем планируется запуск задачи на исполнение. Следовательно,
+необходимость в вызове метода Start() теперь отпадает.
+
+Например, следующий вызов метода StartNew() в рассматривавшихся ранее программах
+приведет к созданию и запуску задачи tsk одним действием.
+
+Task tsk = Task.Factory.StartNew(MyTask);
+
+После этого оператора сразу же начнет выполняться метод MyTask().
+Метод StartNew() оказывается более эффективным в тех случаях, когда задача создается
+и сразу же запускается на исполнение. Поэтому именно такой подход и применяется
+в последующих примерах программ.
+
+*/
+
+//using System;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//class DemoTask
+//{
+//    // Метод, исполняемый как задача.
+//    static void MyTask()
+//    {
+//        Console.WriteLine("MyTask() №" + Task.CurrentId + " запущен");
+
+//        for (int count = 0; count < 10; count++)
+//        {
+//            Thread.Sleep(500);
+//            Console.WriteLine("В методе MyTask() #" + Task.CurrentId + ", подсчет равен " + count);
+//        }
+
+//        Console.WriteLine("MyTask №" + Task.CurrentId + " завершен");
+//    }
+
+//    static void Main()
+//    {
+//        Console.WriteLine("Основной поток запущен.");
+
+//        // Сконструировать объекты двух задач.
+//        Task tsk = Task.Factory.StartNew(MyTask);
+//        Task tsk2 = Task.Factory.StartNew(MyTask);
+
+//        Console.WriteLine("Идентификатор задачи tsk:" + tsk.Id);
+//        Console.WriteLine("Идентификатор задачи tsk2:" + tsk2.Id);
+
+//        // Приостановить выполнение метода Main() до тех пор,
+//        // пока не завершатся обе задачи tsk и tsk2
+//        Task.WaitAny(tsk, tsk2);
+
+//        Console.WriteLine("Основной поток завершен.");
+//    }
+//}
+
+#endregion
+
+#region English
+
+/*
+
+Using TaskFactory to Start a Task
+
+The preceding examples are written a bit less efficiently than they need to be because it is
+possible to create a task and start its execution in a single step by calling the StartNew()
+method defined by TaskFactory. TaskFactory is a class that provides various methods that
+streamline the creation and management of tasks. The default TaskFactory can be obtained
+from the read-only Factory property provided by Task. Using this property, you can call any
+of the TaskFactory methods.
+
+There are many forms of StartNew(). The simplest version is shown here:
+
+public Task StartNew(Action action)
+
+Here, action is the entry point to the task to be executed. StartNew() automatically creates a
+Task instance for action and then starts the task by scheduling it for execution. Thus, there is
+no need to call Start().
+
+For example, assuming the preceding programs, the following call creates and starts tsk
+in one step:
+
+Task tsk = Task.Factory.StartNew(MyTask);
+
+After this statement executes, MyTask will begin executing.
+
+Since StartNew() is more efficient when a task is going to be created and then
+immediately started, subsequent examples will use this approach.
+
+*/
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class DemoTask
+{
+    // A method to be run as a task.
+    static void MyTask()
+    {
+        Console.WriteLine("MyTask() #" + Task.CurrentId + " starting");
+
+        for (int count = 0; count < 10; count++)
+        {
+            Thread.Sleep(500);
+            Console.WriteLine("In MyTask() #" + Task.CurrentId + ", count is " + count);
+        }
+
+        Console.WriteLine("MyTask #" + Task.CurrentId + " terminating");
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Main thread starting.");
+
+        // Construct two tasks.
+        Task tsk = Task.Factory.StartNew(MyTask);
+        Task tsk2 = Task.Factory.StartNew(MyTask);
+
+        Console.WriteLine("Task ID for tsk is " + tsk.Id);
+        Console.WriteLine("Task ID for tsk2 is " + tsk2.Id);
+
+        // Suspend Main() until both tsk and tsk2 finish.
+        Task.WaitAny(tsk, tsk2);
+
+        Console.WriteLine("Main thread ending.");
+    }
+}
+
+#endregion
